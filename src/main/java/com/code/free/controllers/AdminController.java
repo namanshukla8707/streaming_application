60c217dbeab008be9a0618d2c6540034747ac727
JaiswalHarsh321 <connect.harsh17@gmail.com>
2025-11-22 15:57:26 +0000

    Admin All users api

---
+++ src/main/java/com/code/free/controllers/AdminController.java
@@ -0,0 +1,28 @@
+package com.code.free.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.code.free.entities.UserEntity;
+import com.code.free.services.AdminService.AdminService;
+
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RequestMapping("/api/v1/admin")
+@RestController
+@RequiredArgsConstructor
+public class AdminController {
+
+    private final AdminService adminService;
+
+    @GetMapping("/users")
+    public ResponseEntity<List<UserEntity>> getUsers() {
+        return ResponseEntity.ok(adminService.getUsers());
+    }
+
+}
